5cb30e01fdffe72eac70b25542d0fdab87654a1c
Mitchell Buckley <mitchell.alan.buckley@gmail.com>
2021-03-22 13:36:34 +1100

    some notes on definitions
    Signed-off-by: Mitchell Buckley <mitchell.alan.buckley@gmail.com>

--- docs/Style.thy
+++ docs/Style.thy
@@ -20,6 +20,21 @@ section \<open>General Principles\<close>
 
 section \<open>Definitions\<close>
 
+(* Definitions should be written over 2 lines or 4 lines in one of the following two styles.
+
+   Note that indentation is 2 spaces and the definitions do not run past 100 character line
+   limit. Note also that the \equiv symbol is used.
+
+   FIXME: Do others agree? Mitch.
+ *)
+
+definition
+  hd_opt :: "'a list \<Rightarrow> 'a option"
+where
+  "hd_opt \<equiv> case_list None (\<lambda>h t. Some h)"
+
+definition hd_opt2 :: "'a list \<Rightarrow> 'a option" where
+  "hd_opt2 \<equiv> case_list None (\<lambda>h t. Some h)"
 
 section \<open>Lemma Statements\<close>
 
