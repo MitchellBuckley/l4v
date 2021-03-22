07be3e96326ac28f19339b708a5d1fe2536a6898
Mitchell Buckley <mitchell.alan.buckley@gmail.com>
2021-03-22 13:11:00 +1100

    Add docs/Style.thy
    This is the starting point for a theory file which contains a style guide.
    
    Signed-off-by: Mitchell Buckley <mitchell.alan.buckley@gmail.com>

---
+++ docs/Style.thy
@@ -0,0 +1,30 @@
+(*
+ * Copyright 2019, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ *)
+
+chapter \<open>An Isabelle Syntax Style Guide\<close>
+
+theory Style
+  imports Main
+begin
+
+
+section \<open>General Principles\<close>
+
+
+section \<open>Definitions\<close>
+
+
+section \<open>Lemma Statements\<close>
+
+
+section \<open>Proofs\<close>
+
+
+end
