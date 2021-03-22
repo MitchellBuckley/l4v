48ba1fb8bfb3724f704a565293c31b6bd3958a6b
Mitchell Buckley <mitchell.alan.buckley@gmail.com>
2021-03-22 14:09:33 +1100

    Some extended instructions on definitions.
    Signed-off-by: Mitchell Buckley <mitchell.alan.buckley@gmail.com>

--- docs/Style.thy
+++ docs/Style.thy
@@ -20,21 +20,44 @@ section \<open>General Principles\<close>
 
 section \<open>Definitions\<close>
 
-(* Definitions should be written over 2 lines or 4 lines in one of the following two styles.
+(* Definitions should be written over 2 lines in the following two style.
 
    Note that indentation is 2 spaces and the definitions do not run past 100 character line
    limit. Note also that the \equiv symbol is used.
-
-   FIXME: Do others agree? Mitch.
  *)
 
-definition
-  hd_opt :: "'a list \<Rightarrow> 'a option"
-where
+definition hd_opt :: "'a list \<Rightarrow> 'a option" where
   "hd_opt \<equiv> case_list None (\<lambda>h t. Some h)"
 
-definition hd_opt2 :: "'a list \<Rightarrow> 'a option" where
-  "hd_opt2 \<equiv> case_list None (\<lambda>h t. Some h)"
+(* In the case that the type_signature causes an overflow of the 100 char line limit,
+   use the following pattern.
+
+   Note that the indentation of the type signature remains no less than the indentation level of
+   the initial `"`.
+ *)
+
+type_synonym my_very_long_nat_type = nat
+type_synonym 'a my_very_long_option_type = "'a option"
+type_synonym 'a my_very_long_list_type = "'a list"
+definition hd_opt2 :: "'a my_very_long_list_type \<Rightarrow> my_very_long_nat_type \<Rightarrow>
+                       'a my_very_long_list_type \<Rightarrow> 'a my_very_long_option_type" where
+  "hd_opt2 dummy_list dummy_nat \<equiv> case_list None (\<lambda>h t. Some h)"
+
+(* In the case that the definition body causes an overflow of the 100 char line limit, use one
+   of the following two patterns.
+
+   Note that in each case, indentation of the right-hand expression remains no less than the
+   indentation level of `case_list`.
+ *)
+
+definition hd_opt3 :: "'a list \<Rightarrow> 'a option" where
+  "hd_opt3 \<equiv> case_list
+             None
+             (\<lambda>h t. Some h)"
+
+definition hd_opt4 :: "'a list \<Rightarrow> 'a option" where
+  "hd_opt4 \<equiv>
+   case_list None (\<lambda>h t. Some h)"
 
 section \<open>Lemma Statements\<close>
 
