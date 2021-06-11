d54bb5474fdb46c5152533df3f1e0c6840de8697
Mitchell Buckley <mitchell.alan.buckley@gmail.com>
2021-06-11 13:14:25 +1000

    tweak script again
    Signed-off-by: Mitchell Buckley <mitchell.alan.buckley@gmail.com>

--- sr2.sh
+++ sr2.sh
@@ -5,7 +5,7 @@ do_replace () {
 if ! grep -r -w $2_corres .;
   then
   # search and replace
-  find . -type f -exec sed -i "s/\b$1\b/$2_corres/g" {} +
+  find . -type f -exec sed -i "s/\b$1\b/$2/g" {} +
   # commit the change
   #git commit -am "rename $1 to $2_corres"
 else
